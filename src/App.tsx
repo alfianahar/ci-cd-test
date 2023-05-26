a439ddc8b8f8802a45e11ee0641f16a3b80e10ff
Alfian Nahar <58506749+alfianahar@users.noreply.github.com>
2023-05-26 14:15:39 +0700

    add: footer

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,10 @@ function App() {
       <div className="flex h-screen flex-col items-center justify-center">
         <h1 className="font-sans text-lg font-bold">Simple Login Form</h1>
         <LoginForm />
+        <footer>Made by Alfian Nahar</footer>
       </div>
     </>
   );
 }
-
+o;
 export default App;
